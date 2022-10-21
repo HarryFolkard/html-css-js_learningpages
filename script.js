f743bcfb559223c446db34c9c274f6edbf63a483
HarryFolkard <folkardharry@gmail.com>
2022-10-21 18:21:20 +0100

    coursera_wk4

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ a();
 
 /*Functions*/
 function compare (x, y) {
-    return x > y;
+  return x > y;
 }
 var a = compare(4,5);
 /*so here we are defining a function called compare which returns the highest number of two numbers.
@@ -60,3 +60,72 @@ js has seven built in types*/
     var x; 
     /*== undefined. note that undefined and not defined are two different things!! here x has been declared but has no value,
     so it is undefined. if the variable had never been declared it would be not defined.*/
+
+/*String Concatination*/
+var string = "Hello";
+string += " World";
+console.log(string + "!");
+/*So here the use of += adds "World" to our string that already conatins "Hello"*/
+
+// Maths Operators
+console.log((5 + 4) / 3);
+// these work pretty much exactly as you would expect they do.
+
+// Equality
+var x = 4, y = 4;
+if (x == y) {
+  console.log("x=4 is equality to y=4");
+}
+// just use double equals signsl - nothing special here!
+
+// Strict Equality
+if (x === y) {
+  console.log("Strict: x=4 is equal to y=4");
+}
+else {
+  console.log("Strict: x=4 is not equal to y=4")
+}
+// the triple equals sign stops js from doing 'type conversion'
+// e.g. converting x = "4" (a string) to x = 4 (an integer).
+
+// Truth and Falsity
+if ( false || null || undefined || "" || 0 || NaN) {
+  console.log("This won't ever execute");
+}
+if ( true && "hello" && 1 && -1 && "false") {
+  console.log("All true");
+}
+// || means or, && means and 
+
+// For loop
+var sum = 0;
+for (var i = 0; i < 10; i++) {
+  sum = sum + 1
+}
+console.log("sum of 0 through 9 is: " + sum);
+
+// Default values
+function orderChickenWith(sideDish) {
+  sideDish = sideDish || "nothing";
+  console.log("Chicken with " + sideDish);
+}
+orderChickenWith("noodles");
+orderChickenWith();
+// so here the || is a shortcut since if the user didn't enter a side dish, rather than
+// js noting there is an undefined variable and forcing through the string "undefined" to print
+// we just get the output "Chicken with nothing".
+
+// Object Creation
+var company = new Object();
+company.name = "Tesco";
+company.ce = new Object();
+company.ceo.firstName = "John";
+company.ceo.favColour = "blue";
+
+console.log(company["name"]);
+console.log(company.name);
+// why are there two ways of calling the values of the object?
+// just incase you want to use special signs or spaces or a variable then use the bracket notation
+company["stock of company"] = 110;
+
+// Neater Object Creation
